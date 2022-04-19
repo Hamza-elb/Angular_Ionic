44d422d182ac8041d42ea99b82bb909c0647a09f
EL-BOURISSI Hamza <hamza.elbourissi.10@gmail.com>
2022-04-19 15:08:29 +0000

    modif3
    modif3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular/providers/nav-controller';
-import { DetailsPage } from '../details/details.page';
+
 
 @Component({
   selector: 'app-home',
@@ -10,25 +10,13 @@ import { DetailsPage } from '../details/details.page';
 })
 export class HomePage {
 
-  Formations : any[];
+  
 
-  constructor(public router : Router) {
+  constructor() {
 
-    this.Formations = [
-
-      {nom : 'Angular'  , prix : 2000, detail : 'lorem ipsum'},
-      {nom : 'Ionic'  ,   prix : 2500,  detail : 'lorem ipsum'},
-      {nom : 'Laravel', prix : 3500, detail : 'lorem ipsum' },
-      {nom : 'Php',     prix : 2000,  detail : 'lorem ipsum'}
     
-    ];
 
   }
- //this.router.navigateByUrl('/DetailsPage',data);
-  showData(data: any) : void {
-      this.router.navigateByUrl('/DetailsPage',data);
-    console.log('Data is ',data);
-  }
-
+ 
   
 }
